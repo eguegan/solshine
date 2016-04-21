a9d7d755926894e9facb6d09991adce1b8c2500c
eguegan <e.guegan@margulls.com>
2016-04-20 23:35:46 -0400

    No more today in list view handled in fragment

--- app/src/main/java/com/example/admin/solshine/ForecastAdapter.java
+++ app/src/main/java/com/example/admin/solshine/ForecastAdapter.java
@@ -49,17 +49,7 @@ public class ForecastAdapter extends CursorAdapter {
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         // Choose the layout type
         int viewType = getItemViewType(cursor.getPosition());
-        int layoutId = -1;
-        switch (viewType) {
-            case VIEW_TYPE_TODAY: {
-                layoutId = R.layout.list_item_forecast_today;
-                break;
-            }
-            case VIEW_TYPE_FUTURE_DAY: {
-                layoutId = R.layout.list_item_forecast;
-                break;
-            }
-        }
+        int layoutId = R.layout.list_item_forecast;
 
         View view = LayoutInflater.from(context).inflate(layoutId, parent, false);
 
