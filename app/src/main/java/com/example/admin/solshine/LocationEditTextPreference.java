12dc656d5027071cd88787883dc6cf9852a755b8
eguegan <e.guegan@margulls.com>
2016-05-08 16:10:04 -0400

    add place location shared preferences

--- app/src/main/java/com/example/admin/solshine/LocationEditTextPreference.java
+++ app/src/main/java/com/example/admin/solshine/LocationEditTextPreference.java
@@ -1,5 +1,6 @@
 package com.example.admin.solshine;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -17,6 +18,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.ui.PlacePicker;
 
 /**
  * Created by admin on 5/1/2016.
@@ -36,14 +40,16 @@ public class LocationEditTextPreference extends EditTextPreference {
         } finally {
             a.recycle();
         }
-    }
 
-    GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
-    int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
-
-    if(resultCode == ConnectionResult.SUCCESS) {
-        // Add the get current location widget to our location preference
-        setWidgetLayoutResource(R.layout.pref_current_location);
+        // Check to see if Google Play services is available. The Place Picker API is available
+        // through Google Play services, so if this is false, we'll just carry on as though this
+        // feature does not exist. If it is true, however, we can add a widget to our preference.
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(getContext());
+        if (resultCode == ConnectionResult.SUCCESS) {
+            // Add the get current location widget to our location preference
+            setWidgetLayoutResource(R.layout.pref_current_location);
+        }
     }
 
     @Override
@@ -53,8 +59,33 @@ public class LocationEditTextPreference extends EditTextPreference {
         currentLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // We'll use a toast for now so that we can test our new preference widget.
-                Toast.makeText(getContext(), "Woo!", Toast.LENGTH_LONG).show();
+                Context context = getContext();
+
+                // Launch the Place Picker so that the user can specify their location, and then
+                // return the result to SettingsActivity.
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+
+
+                // We are in a view right now, not an activity. So we need to get ourselves
+                // an activity that we can use to start our Place Picker intent. By using
+                // SettingsActivity in this way, we can ensure the result of the Place Picker
+                // intent comes to the right place for us to process it.
+                Activity settingsActivity = (SettingsActivity) context;
+                try {
+                    settingsActivity.startActivityForResult(
+                            builder.build(context), SettingsActivity.PLACE_PICKER_REQUEST);
+
+                } catch (GooglePlayServicesNotAvailableException
+                        | GooglePlayServicesRepairableException e) {
+                    // What did you do?? This is why we check Google Play services in onResume!!!
+                    // The difference in these exception types is the difference between pausing
+                    // for a moment to prompt the user to update/install/enable Play services vs
+                    // complete and utter failure.
+                    // If you prefer to manage Google Play services dynamically, then you can do so
+                    // by responding to these exceptions in the right moment. But I prefer a cleaner
+                    // user experience, which is why you check all of this when the app resumes,
+                    // and then disable/enable features based on that availability.
+                }
             }
         });
 
