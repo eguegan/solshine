04823f7fd0046dd86fc9863cc368d63424b16b97
eguegan <e.guegan@margulls.com>
2016-05-09 20:44:44 -0400

    add widgets and muzei

---
+++ app/src/main/java/com/etienne/admin/solshine/widget/TodayWidgetProvider.java
@@ -0,0 +1,42 @@
+package com.etienne.admin.solshine.widget;
+
+import android.annotation.TargetApi;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.RemoteViews;
+
+import com.etienne.admin.solshine.MainActivity;
+import com.etienne.admin.solshine.R;
+import com.etienne.admin.solshine.Utility;
+import com.etienne.admin.solshine.sync.SunshineSyncAdapter;
+
+/**
+ * Created by admin on 5/8/2016.
+ */
+public class TodayWidgetProvider extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
+                                          int appWidgetId, Bundle newOptions) {
+        context.startService(new Intent(context, TodayWidgetIntentService.class));
+    }
+
+    @Override
+    public void onReceive(@NonNull Context context, @NonNull Intent intent) {
+        super.onReceive(context, intent);
+        if (SunshineSyncAdapter.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            context.startService(new Intent(context, TodayWidgetIntentService.class));
+        }
+    }
+}
